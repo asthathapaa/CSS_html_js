f94901067860354f9fe866078eb0ca43abb42aa9
Astha Thapa <119739490+asthathapaa@users.noreply.github.com>
2024-05-27 09:01:08 +0545

    Add files via upload

---
+++ CSS/script.js
@@ -0,0 +1,12 @@
+const submitBtn = document.getElementById('submit')
+const closeBtn = document.getElementById('close')
+const popup = document.querySelector('.popupContainer')
+
+submitBtn.addEventListener('click', function () {
+    popup.classList.add('popupActive')
+
+})
+closeBtn.addEventListener('click', function () {
+    popup.classList.remove('popupActive')
+
+})
